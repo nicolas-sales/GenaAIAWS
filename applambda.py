c7de192d273578d92a056a9488eed7f768de9bdf
nicolas-sales <nicolas.sales77@gmail.com>
2026-03-21 19:11:14 +0100

    app lambda

--- applambda.py
+++ applambda.py
@@ -1,6 +1,7 @@
 import boto3
 import botocore.config
 import json
+import datatime
 
 def blog_generate_using_bedrock(blogtopic:str)-> str:
     prompt=f"""                                      
@@ -32,10 +33,38 @@ def blog_generate_using_bedrock(blogtopic:str)-> str:
     except Exception as e:
         print(f"Error generating the blog:{e}")
         return ""
+    
+    
+def save_blog_details_s3(s3_key,s3_bucket,generate_blog):
+    s3=boto3.client("s3")
+
+    try:
+        s3.put_object(Bucket=s3_bucket,key=s3_key,Body=generate_blog)
+        print("Code saved to s3")
+
+    except Exception as e:
+        print("Error when saving the code to s3")
+
 
 def lambda_handler(event, context): # event: données d'entrées, context: infos techniques sur l'exécution
     # TODO implement
+    event=json.loads(event['body'])
+    blogtopic=event['blog_topic']
+
+    generate_blog=blog_generate_using_bedrock(blogtopic=blogtopic)
+
+    if generate_blog:
+        current_time=datatime.now().strftime("%H%M%S")
+        s3_key=f"blog-outputs/{current_time}.txt"
+        s3_bucket="aws_bedrock"
+        save_blog_details_s3(s3_key,s3_bucket,generate_blog)
+
+    else:
+        print('No blog was generated')
+
     return {
-        'statusCode': 200,
-        'body': json.dumps('Hello from Lambda!')
+        "statuscode":200,
+        "body":json.dumps('Blog Generation is completed')
     }
+
+    
